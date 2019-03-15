32ad70d991bd13c2a7f41421e7e427aa052ae4a2
shraey <shraey96@gmail.com>
2019-03-15 11:17:05 +0530

    .

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join('build')));
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
 app.get('/', function (req, res) {
     // res.sendFile(path.join(__dirname, 'build', 'index.html'));
     const index = path.join(__dirname, 'build', 'index.html');
     res.sendFile(index);
+})
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
