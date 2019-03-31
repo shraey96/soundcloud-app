0459020e3afc5d271905835082142ed82a4f2419
shraey <shraey96@gmail.com>
2019-03-31 23:42:51 +0530

    ...

--- src/secret.js
+++ src/secret.js
@@ -5,6 +5,7 @@ const appBase = {
     clientId: isProd ? 'On5DkG8z5mpEGCZzj9tQ99MXXSW1iVT9' : 'AKm0rmaY0ScS4y0FyUdvWMyfmtMdUYh6',
     redirect_uri: isProd ? 'https://shielded-bayou-64930.herokuapp.com/callback' : 'http://localhost:3000/callback'
 }
+
 // const appBase = {
 //     proxyURL: 'https://cryptic-ravine-67258.herokuapp.com/',
 //     clientId: 'On5DkG8z5mpEGCZzj9tQ99MXXSW1iVT9',
